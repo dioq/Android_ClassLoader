eec1227502658f79222ce1feb47dadd0bc806694
dioq <ptttqq@gmail.com>
2025-02-14 17:38:17 +0800

    封装代码,是代码更简洁

--- loader/src/main/java/cn/my/loader/MainActivity.java
+++ loader/src/main/java/cn/my/loader/MainActivity.java
@@ -25,6 +25,11 @@ public class MainActivity extends AppCompatActivity {
     private Context context = null;
     private final String dir = "/data/local/tmp";
 
+
+    private String dexPath = null;
+    private File dexUnzipFile = null;
+    private String soPath = null;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,21 +37,24 @@ public class MainActivity extends AppCompatActivity {
         showText = findViewById(R.id.textBloardId);
 
         context = MainActivity.this;
+        PermissionUtils.getInstance().requestPermissions(context);
+
+        dexPath = new File(dir, "classes.dex").getPath();
+        dexUnzipFile = context.getDir("dex", 0);
+        soPath = context.getCacheDir().getPath();
+        Log.d(TAG, "dexUnzipFile:" + dexUnzipFile.getPath());
     }
 
     public void load1(View view) {
-        File dexOutputDir = this.getDir("dex", 0);// 无法直接从外部路径加载.dex文件，需要指定APP内部路径作为缓存目录（.dex文件会被解压到此目录）
-        Log.d(TAG, "dexOutputDir:" + dexOutputDir.getPath());
-
-        File dexFile = new File(dir, "classes.dex");
-        String libSearchPath = context.getFilesDir().getPath();
-
-        //1.待加载的dex文件路径，如果是外存路径，一定要加上读外存文件的权限,
-        //2.解压后的dex存放位置，此位置一定要是可读写且仅该应用可读写
-        //3.指向包含本地库(so)的文件夹路径，可以设为null
-        //4.父级类加载器，一般可以通过context.getClassLoader()获取到，也可以通过ClassLoader.getSystemClassLoader()取到。
-//        ClassLoader classLoader = new DexClassLoader(dexFile.getPath(), dexOutputDir.getAbsolutePath(), libSearchPath, getClassLoader());
-        ClassLoader classLoader = new BaseDexClassLoader(dexFile.getPath(), dexOutputDir, libSearchPath, getClassLoader());
+        /*
+            参数:
+            1.待加载的dex文件路径，如果是外存路径，一定要加上读外存文件的权限,
+            2.解压后的dex存放位置，此位置一定要是可读写且仅该应用可读写
+            3.指向包含本地库(so)的文件夹路径，可以设为null
+            4.父级类加载器，一般可以通过context.getClassLoader()获取到，也可以通过ClassLoader.getSystemClassLoader()取到。
+        * */
+        ClassLoader classLoader = new BaseDexClassLoader(dexPath, dexUnzipFile, soPath, ClassLoader.getSystemClassLoader());
+//        ClassLoader classLoader = new DexClassLoader(dexPath, dexUnzipFile.getAbsolutePath(), soPath, context.getClassLoader());
         invokeCustomMethod(classLoader);
     }
 
@@ -59,39 +67,19 @@ public class MainActivity extends AppCompatActivity {
         byteBuffers.position(0);
 
         //            classLoader = new InMemoryDexClassLoader(byteBuffers, null, context.getClassLoader());
-        ClassLoader classLoader = new InMemoryDexClassLoader(byteBuffers, context.getClassLoader());
+        ClassLoader classLoader = new InMemoryDexClassLoader(byteBuffers, ClassLoader.getSystemClassLoader());
         invokeCustomMethod(classLoader);
     }
 
     public void load3(View view) {
-        File dexOutputDir = this.getDir("dex", 0);// 无法直接从外部路径加载.dex文件，需要指定APP内部路径作为缓存目录（.dex文件会被解压到此目录）
-        Log.d(TAG, "dexOutputDir:" + dexOutputDir.getPath());
+        File apkFile = new File(dir, "makedex-debug.apk");
 
-        File dexFile = new File(dir, "makedex-debug.apk");
-        String libSearchPath = context.getFilesDir().getPath();
-
-        ClassLoader classLoader = new BaseDexClassLoader(dexFile.getPath(), dexOutputDir, libSearchPath, getClassLoader());
-        invokeCustomMethod2(classLoader);
+//        ClassLoader classLoader = new BaseDexClassLoader(apkFile.getPath(), dexUnzipFile, soPath, ClassLoader.getSystemClassLoader());
+        ClassLoader classLoader = new DexClassLoader(apkFile.getPath(), dexUnzipFile.getAbsolutePath(), soPath, ClassLoader.getSystemClassLoader());
+        invokeCustomMethod(classLoader);
     }
 
     public void invokeCustomMethod(ClassLoader classLoader) {
-        try {
-            // 获取类
-            Class<?> clz = classLoader.loadClass("cn.my.study.Test");
-
-            // 反射调用方法
-            Method method = clz.getMethod("getMsgFromDexFile", String.class);
-            Object result = method.invoke(clz.newInstance(), "TestMsg");
-            String msg = String.format("result:%s", result);
-            Log.d(TAG, msg);
-            showText.setText(msg);
-        } catch (ClassNotFoundException | NoSuchMethodException | IllegalAccessException |
-                 InstantiationException | InvocationTargetException e) {
-            Log.d(TAG, "Exception:" + e.getMessage());
-        }
-    }
-
-    public void invokeCustomMethod2(ClassLoader classLoader) {
         try {
             // 获取类
             Class<?> clz = classLoader.loadClass("cn.my.dex.Start");
