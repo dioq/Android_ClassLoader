ddb574274ae6768f952d15996c5f3b95ea3635d9
dioq <ptttqq@gmail.com>
2025-02-19 16:57:21 +0800

    代码调试

--- loader/README.md
+++ loader/README.md
@@ -10,3 +10,14 @@ BaseDexClassLoader继承自ClassLoader,是抽象类ClassLoader的具体实现类
 PathClassLoader加载系统类和应用程序的类,如果是加载非系统应用程序类,则会加载data/app/目录下的dex文件以及包含dex的apk文件或jar文件
 DexClassLoader 可以加载自定义的dex文件以及包含dex的apk文件或jar文件,也支持从SD卡进行加载
 InMemoryDexClassLoader是Android8.0新增的类加载器,继承自BaseDexClassLoader,用于加载内存中的dex文件。
+
+例如:
+Activity的类加载器是BootClassLoader
+MainActivity,AppcompatActivity类的加载器是PathClassLoader
+
+## 加载过程
+
+对于任意一个类,都需要由它的类加载器和这个类本身一同确定其在就Java虚拟机中的唯一性,即使两个类来源于同一个Class文件,只要加载它们的类加载器不同,那这两个类就必定不相等。
+这里的"相等"包括了代表类的Class对象的equals()、isAssignableFrom()、isInstance()等方法的返回结果,也包括了使用instanceof关键字对对象所属关系的判定结果。
+
+如果一个类加载器收到了加载类的请求,它不会自己立即去加载类,它会先去请求父类加载器,每个层次的类加载器都是如此。层层传递,直到传递到最高层的类加载器,只有当父类加载器反馈自己无法加载这个类,才会有当前子类加载器去加载该类。
--- loader/src/main/java/cn/my/loader/MainActivity.java
+++ loader/src/main/java/cn/my/loader/MainActivity.java
@@ -17,6 +17,7 @@ import java.nio.ByteBuffer;
 import dalvik.system.DexClassLoader;
 import dalvik.system.BaseDexClassLoader;
 import dalvik.system.InMemoryDexClassLoader;
+import dalvik.system.PathClassLoader;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,8 +29,8 @@ public class MainActivity extends AppCompatActivity {
 
 
     private File dexFile = null;
-    private File dexUnzipFile = null;
-    private String soPath = null;
+    private File optimizedDirectory = null;
+    private String librarySearchPath = null;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -42,9 +43,9 @@ public class MainActivity extends AppCompatActivity {
         PermissionUtils.getInstance().requestPermissions(context);
 
         dexFile = new File(dir, "classes.dex");
-        dexUnzipFile = context.getDir("dex", 0);
-        soPath = context.getCacheDir().getPath();
-        Log.d(TAG, "dexUnzipFile:" + dexUnzipFile.getPath());
+        optimizedDirectory = context.getDir("dex", 0);
+        librarySearchPath = context.getCacheDir().getPath();
+        Log.d(TAG, "optimizedDirectory:" + optimizedDirectory.getPath());
     }
 
     @SuppressLint("SetTextI18n")
@@ -52,12 +53,12 @@ public class MainActivity extends AppCompatActivity {
         /*
             参数:
             1.待加载的dex文件路径,如果是外存路径,一定要加上读外存文件的权限
-            2.解压后的dex存放位置,此位置一定要是可读写且仅该应用可读写
+            2.odex(优化的dex)存放的路径,此位置一定要是可读写且仅该应用可读写
             3.指向包含本地库(so)的文件夹路径,可以设为null
             4.父级类加载器,一般可以通过context.getClassLoader()获取到,也可以通过ClassLoader.getSystemClassLoader()取到
         **/
-        ClassLoader classLoader = new BaseDexClassLoader(dexFile.getPath(), dexUnzipFile, soPath, ClassLoader.getSystemClassLoader());
-//        ClassLoader classLoader = new DexClassLoader(dexFile.getPath(), dexUnzipFile.getPath(), soPath, context.getClassLoader());
+//        ClassLoader classLoader = new BaseDexClassLoader(dexFile.getPath(), optimizedDirectory, librarySearchPath, ClassLoader.getSystemClassLoader());
+        ClassLoader classLoader = new DexClassLoader(dexFile.getPath(), optimizedDirectory.getPath(), librarySearchPath, context.getClassLoader());
         invokeCustomMethod(classLoader);
         tv.setText("从dex文件中加载类");
     }
@@ -79,10 +80,19 @@ public class MainActivity extends AppCompatActivity {
     public void load3(View view) {
         File apkFile = new File(dir, "makedex-debug.apk");
 
-//        ClassLoader classLoader = new BaseDexClassLoader(apkFile.getPath(), dexUnzipFile, soPath, ClassLoader.getSystemClassLoader());
-        ClassLoader classLoader = new DexClassLoader(apkFile.getPath(), dexUnzipFile.getPath(), soPath, ClassLoader.getSystemClassLoader());
+        ClassLoader classLoader = new BaseDexClassLoader(apkFile.getPath(), optimizedDirectory, librarySearchPath, ClassLoader.getSystemClassLoader());
+//        ClassLoader classLoader = new DexClassLoader(apkFile.getPath(), optimizedDirectory.getPath(), librarySearchPath, ClassLoader.getSystemClassLoader());
         invokeCustomMethod(classLoader);
         tv.setText("从apk文件中加载类");
+    }
+
+    @SuppressLint({"SetTextI18n", "SdCardPath"})
+    public void load4(View view) {
+        File dexFile = new File("/sdcard/Download/", "classes.dex");
+//        File dexFile = new File("/sdcard/Download/", "makedex-debug.apk"); // or
+        ClassLoader classLoader = new PathClassLoader(dexFile.getPath(), ClassLoader.getSystemClassLoader());
+        invokeCustomMethod(classLoader);
+        tv.setText("PathClassLoader 加载类");
     }
 
     public void invokeCustomMethod(ClassLoader classLoader) {
